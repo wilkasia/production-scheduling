94053b1c9b8921c62fc70d25fed4c0c5dfc4562d
Wojciech Wilanski <wwilanski@gmail.com>
2021-06-27 14:29:50 +0200

    new folders

---
+++ main.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3.8
+# -*- coding: utf-8 -*-
+
+import os
+from lib.Rules import Rules
+
+
+def main():
+    print('CWD')
+    print(os.getcwd() + "/data")
+    data = os.getcwd() + "/data/"
+
+    r = Rules(data + "prod-stal.csv")
+    r.get_rules(data + "rules-stal.csv", data + "types-stal.csv")
+    r.print_brands_list()
+
+
+if __name__ == '__main__':
+    main()
